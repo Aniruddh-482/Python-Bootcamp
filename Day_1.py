822b362e3c3c1cfd33bee6e8b8e7a077c626bf7a
Aniruddh Upadhyay <72350503+Aniruddh-482@users.noreply.github.com>
2021-02-24 10:43:01 +0530

    Create Day_1.py

---
+++ Day_1.py
@@ -0,0 +1,50 @@
+#DAY-01 (100 Days of Code Python)
+#Print 'Hello World!'
+print("Hello World!!")
+
+#Python Print Function
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#'\n' Keyword
+print("Hello World!!\nHello World!!")
+
+#Concatination
+print("Hello"+"Ansh")
+print("Hello"+" Ansh")
+print("Hello "+"Ansh")
+print("Hello"+" "+"Ansh")
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#Input Function
+print("Hello "+input('What is your Name: ')+"!")
+#Excercise
+s=input("What is your name: ")
+n=len(s)
+print(n)
+#or
+print(len(input("What is your name: ")))
+
+#Python Variables
+name = input("What is your name: ")
+print(name)
+name = "Ansh"
+print(name)
+
+name = input("What is your name: ")
+length = len(name)
+print(length)
+
+#Swapping
+a = input("a: ")
+b = input("b: ")
+c = a
+a = b
+b = c
+print("a: " + a)
+print("b: " + b)
