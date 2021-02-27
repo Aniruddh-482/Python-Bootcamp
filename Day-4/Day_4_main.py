5ad4249c990c285233a5813c390f01e62c1f8d66
Aniruddh Upadhyay <72350503+Aniruddh-482@users.noreply.github.com>
2021-02-27 23:17:26 +0530

    Create Day_4_main.py

---
+++ Day-4/Day_4_main.py
@@ -0,0 +1,12 @@
+#Randomisation
+#Python random module
+#https://www.askpython.com/python-modules/python-random-module-generate-random-numbers-sequences
+#https://www.khanacademy.org/computing/computer-science/cryptography/crypt/v/random-vs-pseudorandom-number-generators
+import random
+import my_module
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+
+print(my_module.pi)
