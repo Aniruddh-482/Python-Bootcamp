acc71a6764fe0d7e74e1b7468872100d77da0998
Aniruddh Upadhyay <72350503+Aniruddh-482@users.noreply.github.com>
2021-03-01 00:14:41 +0530

    Update Day_4

--- Day-4/Day_4_main.py
+++ Day-4/Day_4_main.py
@@ -130,8 +130,21 @@ column = round(int(position) / 10)
 row = int(position) % 10
 column -= 1
 row -= 1
-map[row][column] = 'x'
+map[row][column] = 'X'
 print(f"{row1}\n{row2}\n{row3}")
+#or
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+horizontal = int(position[0])  #2
+vertical = int(position[1])  #3
+selected_row = map[vertical - 1]
+selected_row[horizontal - 1] = 'X'
+print(f"{row1}\n{row2}\n{row3}")
+#Output ==>
 # ['⬜️', '⬜️', '⬜️']
 # ['⬜️', '⬜️', '⬜️']
 # ['⬜️', '⬜️', '⬜️']
@@ -139,9 +152,6 @@ print(f"{row1}\n{row2}\n{row3}")
 # ['⬜️', '⬜️', '⬜️']
 # ['⬜️', '⬜️', '⬜️']
 # ['⬜️', 'x', '⬜️']
-
-
-
 
 
 
