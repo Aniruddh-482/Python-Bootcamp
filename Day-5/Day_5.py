9e2ba5da369146447b7b205641219da9f2ef74fb
Aniruddh Upadhyay <72350503+Aniruddh-482@users.noreply.github.com>
2021-03-03 01:45:49 +0530

    Update Day_5

--- Day-5/Day_5.py
+++ Day-5/Day_5.py
@@ -89,6 +89,66 @@ print(total)
 # prints sum of all even numbers from 1 to 100 : 2550
 
 
+# You are going to write a program that automatically prints the solution to the FizzBuzz game.
+# Your program should print each number from 1 to 100 in turn.
+# When the number is divisible by 3 then instead of printing the number it should print "Fizz".
+# When the number is divisible by 5, then instead of printing the number it should print "Buzz".
+# And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz".
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 ==0:  #Divisible by both 3 and 5
+    print("FizzBuzz")
+  elif number % 3 == 0:  #Divisible by 3
+    print("Fizz")
+  elif number % 5 == 0:  #Divisible by 5
+    print("Buzz")
+  else:
+    print(number) 
+
+
+# Create a Password Generator
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+password = ""
+for char in range(1, nr_letters + 1):
+  # random_char = random.choice(letters)
+  # # print(random_char)
+  # password = password + random_char
+  # print(password)
+  password += random.choice(letters)
+for char in range(1, nr_symbols + 1):
+  password += random.choice(symbols)
+for char in range(1, nr_numbers + 1):
+  password += random.choice(numbers)
+print(password)
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+password_list = []
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+# print(password_list)
+random.shuffle(password_list)
+# print(password_list)
+password = ""
+for char in password_list:
+  password += char
+print(f"Your Password is: {password}")
+
 
 
 
