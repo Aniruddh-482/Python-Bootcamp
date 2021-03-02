6c4bcd0251421123bee33d99ed9ad314535a7782
Aniruddh Upadhyay <72350503+Aniruddh-482@users.noreply.github.com>
2021-03-02 19:15:12 +0530

    Update Day_4

--- Day-4/Day_4_main.py
+++ Day-4/Day_4_main.py
@@ -164,7 +164,6 @@ rock = '''
       (____)
 ---.__(___)
 '''
-
 paper = '''
     _______
 ---'   ____)____
@@ -173,7 +172,6 @@ paper = '''
          _______)
 ---.__________)
 '''
-
 scissors = '''
     _______
 ---'   ____)____
@@ -182,13 +180,9 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-
-
-choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. ")
+choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
 choice_int = int(choice)
-
-random_choice = random.randint(0, 3)
-
+random_choice = random.randint(0, 2)
 if choice_int == 0:
   print(rock)
   print("Computer choose:")
@@ -225,13 +219,106 @@ else:
   else:
     print(paper)
     print("!!You Won!!")
-
 #or
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+game_images = [rock, paper, scissors]
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_choice])
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[computer_choice])
+if user_choice >= 3 or user_choice < 0: 
+  print("You typed an invalid number, you lose!") 
+elif user_choice == 0 and computer_choice == 2:
+  print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+  print("You lose")
+elif computer_choice > user_choice:
+  print("You lose")
+elif user_choice > computer_choice:
+  print("You win!")
+elif computer_choice == user_choice:
+  print("It's a draw")
+####### Debugging challenge: #########
+#Try running this code and type 5.
+#It will give you an IndexError and point to line 32 as the issue.
+#But on line 38 we are trying to prevent a crash by detecting
+#any numbers great than or equal to 3 or less than 0.
+#So what's going on?
+#Can you debug the code and fix it?
+#Debug Solution
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+computer_choice = random.randint(0, 2)
+if user_choice >= 3 or user_choice < 0: 
+  print("You typed an invalid number, you lose!") 
+elif user_choice == 0 and computer_choice == 2:
+  print(game_images[user_choice])
+  print("Computer chose:")
+  print(game_images[computer_choice])
+  print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+  print(game_images[user_choice])
+  print("Computer chose:")
+  print(game_images[computer_choice])
+  print("You lose")
+elif computer_choice > user_choice:
+  print(game_images[user_choice])
+  print("Computer chose:")
+  print(game_images[computer_choice])
+  print("You lose")
+elif user_choice > computer_choice:
+  print(game_images[user_choice])
+  print("Computer chose:")
+  print(game_images[computer_choice])
+  print("You win!")
+elif computer_choice == user_choice:
+  print(game_images[user_choice])
+  print("Computer chose:")
+  print(game_images[computer_choice])
+  print("It's a draw")
+#or other solution of debugging challenge
+#Solution: https://repl.it/@appbrewery/rock-paper-scissors-debugged-end
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if user_choice >= 3 or user_choice < 0: 
+    print("You typed an invalid number, you lose!") 
+else:
+    print(game_images[user_choice])
+    computer_choice = random.randint(0, 2)
+    print("Computer chose:")
+    print(game_images[computer_choice])
+    if user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose")
+    elif computer_choice > user_choice:
+        print("You lose")
+    elif user_choice > computer_choice:
+        print("You win!")
+    elif computer_choice == user_choice:
+        print("It's a draw")
 
 
-
-
-
-
-
- 
