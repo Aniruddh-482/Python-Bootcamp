9d0a3383a9d18c7a1fd3c9a64fff12eace56d7a2
Aniruddh Upadhyay <72350503+Aniruddh-482@users.noreply.github.com>
2021-03-04 01:16:59 +0530

    Update Reeborg's World

--- Day-6/Reebogs_World.py
+++ Day-6/Reebogs_World.py
@@ -140,3 +140,41 @@ while number_of_hurdles > 0:
   jump()  # Jumps 6 times
   number_of_hurdles -= 1
 # #!!You Win!!
+
+
+# Reeborg's World Hurdles 2 Challenge
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+# Hurdles race
+# Reeborg has entered a hurdle race, but he does not know in advance how long the race is. 
+# Make him run the course, following a path similar to the one shown, but stopping at the only flag that will be shown after the race has started.
+# What you need to know
+# The functions move() and turn_left().
+# The condition at_goal() and its negation.
+# How to use a while loop.
+# Your program should also be valid for world Hurdles 1.
+while at_goal() != True:
+    jump()  # Jumps untill at_goal() becomes true
+# or 
+while not at_goal():
+  jump
+
+# Reeborg's World Hurdles 3 Challenge
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+# Hurdles race
+# Reeborg has entered a hurdle race. Make him run the course, following the path shown.
+# The position and number of hurdles changes each time this world is reloaded.
+# What you need to know
+# The functions move() and turn_left().
+# The conditions front_is_clear() or wall_in_front(), at_goal(), and their negation.
+# How to use a while loop and an if statement.
+# Your program should also be valid for worlds Hurdles 1 and Hurdles 2.
+while at_goal() != True:
+    if front_is_clear() == True:
+        move()  
+    elif wall_in_front() == True:  #else:
+        jump()
+
+
+        
+        
+        
