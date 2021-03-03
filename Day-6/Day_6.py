00363bf46e85f371b396127aeb1dcc298a29e88f
Aniruddh Upadhyay <72350503+Aniruddh-482@users.noreply.github.com>
2021-03-03 21:29:33 +0530

    Update Day_6

--- Day-6/Day_6.py
+++ Day-6/Day_6.py
@@ -1,1 +1,28 @@
-#upload tomorrow
+# Defining and Calling Functions in Python
+# Functions has parenthesis
+
+# # Built-in Functions
+# print("Hello!")
+# num_char = len("Hello!")
+# print(num_char)
+# # print() is built-in function
+
+# Defining a function
+def my_function():
+  print("Hello")
+  print("Bye")
+my_function()  # Calling of function
+
+# # Defining Functions
+# def function_name():
+#   # do this
+#   # than do this
+#   # finally do this
+# # Calling Functions
+# function_name()
+# # for using the we had to call it
+
+
+
+
+
