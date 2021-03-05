a0e62806ad5d8976f22b9e8b537968615a8e56fa
Aniruddh Upadhyay <72350503+Aniruddh-482@users.noreply.github.com>
2021-03-05 23:59:31 +0530

    Update Day_6

--- Day-6/Day_6.py
+++ Day-6/Day_6.py
@@ -259,3 +259,32 @@ while at_goal() != True:
         move()  
     elif wall_in_front() == True:
         jump()
+
+
+# Reeborg's World Hurdles 4 Challenge
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+# Lost in a maze
+# Reeborg was exploring a dark maze and the battery in its flashlight ran out.
+# Write a program using an if/elif/else statement so Reeborg can find the exit. 
+# The secret is to have Reeborg follow along the right edge of the maze, turning right if it can, 
+# going straight ahead if it can’t turn right, or turning left as a last resort.
+# What you need to know
+# The functions move() and turn_left().
+# Either the test front_is_clear() or wall_in_front(), right_is_clear() or wall_on_right(), and at_goal().
+# How to use a while loop and if/elif/else statements.
+# It might be useful to know how to use the negation of a test (not in Python).
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif wall_on_right():
+        if front_is_clear():
+            move()
+        else:
+            turn_left()
+    else:
+        turn_left()
