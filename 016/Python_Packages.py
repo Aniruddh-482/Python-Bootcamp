4e792d99e1f8116240b73548ef4441e4b51d39d2
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-03-28 00:29:55 +0530

    Create Python_Packages.py

---
+++ 016/Python_Packages.py
@@ -0,0 +1,9 @@
+# PyPi(Python Package Index): https://pypi.org/
+# PrettyTable: https://pypi.org/project/prettytable/
+# PrettyTable-Tutorial: https://code.google.com/archive/p/prettytable/wikis/Tutorial.wiki
+# Pokémon_Pokédex: https://pokemondb.net/pokedex/game/x-y
+
+
+
+
+
