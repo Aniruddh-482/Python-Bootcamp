625eca96b6043f0708e94f4ea175964892c92764
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-02 00:14:13 +0530

    Create Turtle_Challenge_1.py

--- 018/Turtle Challenges/Turtle_Challenge_1.py
+++ 018/Turtle Challenges/Turtle_Challenge_1.py
@@ -1,3 +1,9 @@
+# Important Links:
+# Turtle Graphic Documentation: https://docs.python.org/3/library/turtle.html
+# Turtle Colors: https://cs111.wellesley.edu/labs/lab01/colors
+# Trinket Colors: https://trinket.io/docs/colors
+
+
 #  Turtle Challenge - 01: Draw a Square
 
 # #####Turtle Intro######
@@ -15,14 +21,22 @@
 
 # ######## Challenge 1 - Draw a Square ############
 
-# timmy_the_turtle.left(90)
-# timmy_the_turtle.forward(200)
-# timmy_the_turtle.right(90)
-# timmy_the_turtle.forward(200)
-# timmy_the_turtle.right(90)
-# timmy_the_turtle.forward(200)
+import turtle as t
+
+timmy_the_turtle = t.Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
 
 # OR
+
 ######## Challenge 1 - Draw a Square ############
 import turtle as t
 
