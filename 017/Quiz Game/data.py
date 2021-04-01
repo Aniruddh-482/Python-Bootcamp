eb41d1380972e0fc7ec895b26c265a4c8db4a0a6
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-01 22:16:41 +0530

    Create data.py

---
+++ 017/Quiz Game/data.py
@@ -0,0 +1,74 @@
+# For More Question: https://opentdb.com/
+
+# question_data = [
+#     {"text": "A slug's blood is green.", "answer": "True"},
+#     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+#     {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+#     {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+#     {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
+#              "you are free to take it home to eat.", "answer": "True"},
+#     {"text": "In London, UK, if you happen to die in the House of Parliament, "
+#              "you are entitled to a state funeral.", "answer": "False"},
+#     {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+#     {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+#     {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+#     {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+#     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+#     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+# ]
+
+question_data = [
+    {"category": "Entertainment: Video Games",
+     "type": "boolean", "difficulty": "easy",
+     "question": "The name of the main character of the video game &quot;The Legend of Zelda&quot;, is Zelda.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games",
+     "type": "boolean", "difficulty": "easy",
+     "question": "The Indie Game Development Studio Cing, developers of Hotel Dusk and Last Window, "
+                 "went bankrupt on March 1st, 2010.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Geography",
+     "type": "boolean", "difficulty": "medium",
+     "question": "The flag of South Africa features 7 colours.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Musicals & Theatres",
+     "type": "boolean", "difficulty": "easy",
+     "question": "In the play Oedipus Rex, "
+                 "Oedipus kills his father due to jealousy in loving his mother.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Music",
+     "type": "boolean", "difficulty": "easy",
+     "question": "Eurobeat is primarily an Italian genre of music.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Animals",
+     "type": "boolean", "difficulty": "medium",
+     "question": "The Platypus is a mammal.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Video Games",
+     "type": "boolean", "difficulty": "easy",
+     "question": "The Konami Code is known as "
+                 "Up, Up, Down, Down, Left, Right, Right, Left, B, A, Start.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Animals",
+     "type": "boolean", "difficulty": "easy",
+     "question": "The Killer Whale is considered a type of dolphin.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Science: Mathematics",
+     "type": "boolean", "difficulty": "hard",
+     "question": "pital was the mathematician "
+                 "who created the homonymous rule that uses derivatives to evaluate limits with indeterminations.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Politics",
+     "type": "boolean", "difficulty": "medium",
+     "question": "Helen Clark was the 37th Prime Minister of Australia.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]}
+]
