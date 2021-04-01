d1aca11859a6a12a24199ed528f66b1b8ee7e625
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-02 00:58:46 +0530

    Create Importing_Modules.py
    Importing Modules, Installing Packages,
    and Working with Aliases

---
+++ 018/Importing_Modules.py
@@ -0,0 +1,49 @@
+#### Basic Import ####
+
+# import turtle
+#   \       \
+# Keyword  Module_Name
+
+# For Example: 
+# import turtle
+# tim = turtle.Turtle()
+
+
+#### from...import... ####
+
+#    Module_Name   Thing in Module
+#       \             \
+# from turtle import Turtle
+#   \           \
+# Keyword     Keyword
+
+# For Example: 
+# from turtle import Turtle
+# tim = Turtle()
+# tom = Turtle()
+# terry = Turtle()
+
+#   Module_Name  Everything
+#       \           \
+# from turtle import *
+#   \           \
+# Keyword     Keyword
+
+# For Example: 
+# from turtle import *
+
+#### Aliasing Modules ####
+
+#   Module_Name  Alias Name
+#       \         /  
+# from turtle as t
+#   \          \
+# Keyword    Keyword
+
+# For Example:
+# import turtle as t
+# tim = t.Turtle()
+
+#### Installing Modules ####
+
+# we have had to install the modules which are not already packaged with the standard python library
