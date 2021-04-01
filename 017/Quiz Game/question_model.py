edbbb158673049f6bdb908a2bad80153ac37087d
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-01 22:18:15 +0530

    Create question_model.py

---
+++ 017/Quiz Game/question_model.py
@@ -0,0 +1,9 @@
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+# new_Q = Question("sdfsdf", "False")
+# print(new_Q.text)  # Prints sdfsdf
+
