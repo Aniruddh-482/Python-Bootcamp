fb057d625ee7e3cf905150814c0fb62702566fee
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-02 14:11:45 +0530

    Create Turtle_Intro.py

---
+++ 018/Turtle Challenges/Turtle_Intro.py
@@ -0,0 +1,17 @@
+########## Turtle Intro ##########
+
+import turtle as t
+
+timmy_the_turtle = t.Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.backward(200)
+timmy_the_turtle.right(90)
+timmy_the_turtle.left(180)
+timmy_the_turtle.setheading(0)
+
+# Important Links:
+# Turtle Graphic Documentation: https://docs.python.org/3/library/turtle.html
+# Turtle Colors: https://cs111.wellesley.edu/labs/lab01/colors
+# Trinket Colors: https://trinket.io/docs/colors
