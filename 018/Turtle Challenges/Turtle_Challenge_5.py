0b573e5c8ae2e3b04f06f3edd71d34cb6f992fea
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-03 22:13:48 +0530

    Create Turtle_Challenge_5.py

---
+++ 018/Turtle Challenges/Turtle_Challenge_5.py
@@ -0,0 +1,51 @@
+# Turtle Challenge - 05: Draw a Spirograph
+
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+########## Challenge 5 - Spirograph ##########
+
+should_continue = 0
+tim.speed(20)
+while should_continue < 72:
+    should_continue += 1
+    tim.color(random_color())
+    tim.circle(100)
+    tim.left(5)
+
+screen = t.Screen()
+screen.exitonclick()
+
+# Solution:
+
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+########### Challenge 5 - Spirograph ########
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+draw_spirograph(5)
+
