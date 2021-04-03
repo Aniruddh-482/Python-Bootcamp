302d35d3146c49199e81351a078951fa38e2731d
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-03 22:50:13 +0530

    Create Hirst_Painting_Project_01.py
    The Hirst Painting Project Part 1 - How to
    Extract RGB Values from Images

---
+++ 018/Hirst_Painting/Hirst_Painting_Project_01.py
@@ -0,0 +1,21 @@
+# Colorgram_Documentation: https://pypi.org/project/colorgram.py/
+
+###This code will not work in repl.it as there is no access to the colorgram package here.###
+##We talk about this in the video tutorials##
+import colorgram
+
+rgb_colors = []
+colors = colorgram.extract('image.jpg', 30)
+for color in colors:
+    # rgb_colors.append(color.rgb)
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+
+print(rgb_colors)
+
+# List of colors that are in given image:
+# color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+
