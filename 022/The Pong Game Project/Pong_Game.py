83865ebe96b21698e0a703afa52baa08aa44b3de
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-06 02:14:32 +0530

    Create Pong_Game.py
    > Set up the Main Screen.
    > Create a Paddle that responds to Key Presses.
    > Write the Paddle Class and Create the Second Paddle.
    > Write the Ball Class and Make the Ball Move.
    > Add the Ball Bouncing Logic.
    > How to Detect Collisions with the Paddle.
    > How to Detect when the Ball goes Out of Bounds.
    > Score Keeping and Changing the Ball Speed.

---
+++ 022/The Pong Game Project/Pong_Game.py
@@ -0,0 +1,49 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("My Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # Needs to bounce
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    elif ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect L Paddle misses
+    elif ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
