72df39059053a9f99710c1372a5045180cfd49f6
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-08 02:10:33 +0530

    Add files via upload

--- 024/Mail Merge Project/main.py
+++ 024/Mail Merge Project/main.py
@@ -1,3 +1,12 @@
+# TODO: Create a letter using starting_letter.txt
+#  for each name in invited_names.txt
+#  Replace the [name] placeholder with the actual name.
+#  Save the letters in the folder "ReadyToSend".
+    
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
 PLACEHOLDER = "[name]"
 
 
