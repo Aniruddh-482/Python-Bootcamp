1b43ea11050985f7d90a245e54a95cd40b073fbe
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-08 18:43:51 +0530

    Create Squirrel_Count.py
    > The Great Squirrel Census Data Analysis (with Pandas!).

---
+++ 025/Pandas/Squirrel Count Project/Squirrel_Count.py
@@ -0,0 +1,21 @@
+# 2018 Central Park Squirrel: https://data.cityofnewyork.us/Environment/2018-Central-Park-Squirrel-Census-Squirrel-Data/vfnx-vebw
+
+# Squirrel Count Chellenge
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(gray_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Red", "Black"],
+    "Count": [gray_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+# print(data_dict)
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
+
