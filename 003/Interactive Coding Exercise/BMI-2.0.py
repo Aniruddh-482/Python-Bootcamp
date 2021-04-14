0f01730794d25d75b6371bb996071bf43d2d7b5f
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-14 16:34:09 +0530

    Create BMI-2.0.py
    > [Interactive Coding Exercise] BMI 2.0

---
+++ 003/Interactive Coding Exercise/BMI-2.0.py
@@ -0,0 +1,23 @@
+# Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+# It should tell them the interpretation of their BMI based on the BMI value.
+# Under 18.5 they are underweight
+# Over 18.5 but below 25 they have a normal weight
+# Over 25 but below 30 they are slightly overweight
+# Over 30 but below 35 they are obese
+# Above 35 they are clinically obese.
+
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+bmi = round(weight / height ** 2)
+
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.")
+
