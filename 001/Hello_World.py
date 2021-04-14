4054585098b2f4ef058ba35323dd136ab8626d15
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-14 14:39:31 +0530

    Create Hello_World.py

--- 001/Hello_World.py
+++ 001/Hello_World.py
@@ -1,16 +1,16 @@
-#DAY-01 (100 Days of Code Python)
-#Print 'Hello World!'
+# DAY-01 (100 Days of Code Python)
+# Print 'Hello World!'
 print("Hello World!!")
 
-#Python Print Function
+# Python Print Function
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
 
-#'\n' Keyword
+# '\n' Keyword
 print("Hello World!!\nHello World!!")
 
-#Concatination
+# Concatination
 print("Hello"+"Ansh")
 print("Hello"+" Ansh")
 print("Hello "+"Ansh")
@@ -21,16 +21,16 @@ print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
 
-#Input Function
+# Input Function
 print("Hello "+input('What is your Name: ')+"!")
 #Excercise
 s=input("What is your name: ")
 n=len(s)
 print(n)
-#or
+# or
 print(len(input("What is your name: ")))
 
-#Python Variables
+# Python Variables
 name = input("What is your name: ")
 print(name)
 name = "Ansh"
@@ -40,7 +40,7 @@ name = input("What is your name: ")
 length = len(name)
 print(length)
 
-#Swapping
+# Swapping
 a = input("a: ")
 b = input("b: ")
 c = a
@@ -48,7 +48,4 @@ a = b
 b = c
 print("a: " + a)
 print("b: " + b)
-
-#We can make multiple lines by simply select them and press 'ctrl + /'
-
 
