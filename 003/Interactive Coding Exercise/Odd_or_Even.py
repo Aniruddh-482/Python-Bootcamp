12e2576408a850ce8e0e715e8e16f43044273d70
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-14 16:27:17 +0530

    Create Odd_or_Even.py
    [Interactive Coding Exercise] Odd or Even? Introducing the Modulo.

---
+++ 003/Interactive Coding Exercise/Odd_or_Even.py
@@ -0,0 +1,10 @@
+# Write a program that works out whether if a given number is an odd or even number.
+
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+  print("This is an even number")
+else:
+  print("This is an odd number")
+  
+  
