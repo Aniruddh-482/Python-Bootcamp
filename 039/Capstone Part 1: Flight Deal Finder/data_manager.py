e83831d0300bc6f0a2dd20c8647f4d0951aa7ce2
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-24 23:21:34 +0530

    Create data_manager.py

---
+++ 039/Capstone Part 1: Flight Deal Finder/data_manager.py
@@ -0,0 +1,33 @@
+import requests
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/82cbcc5f95e037c72da5e3c462132a4e/flightDeals/prices"
+
+
+class DataManager:
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        # 2. Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        # 3. Try importing pretty print and printing the data out again using pprint() to see it formatted.
+        # pprint(data)
+        return self.destination_data
+
+    # 6. In the DataManager Class make a PUT request and use the row id from sheet_data
+    # to update the Google Sheet with the IATA codes. (Do this using code).
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
