3c5dfaecf4b0199567a36c7ddbd1cb0d7c395c3a
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-05-03 00:07:47 +0530

    Create main.py

---
+++ 045/Scraping a Live Website/main.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://news.ycombinator.com/news")
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+# print(soup.title)
+articles = soup.find_all(name="a", class_="storylink")
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
+
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+
+# print(article_texts)
+# print(article_links)
+# print(article_upvotes)
+
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+
+print(article_texts[largest_index])
+print(article_links[largest_index])
+
