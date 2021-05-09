8dfd595209a718cc121f05e01d317a3465705e29
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-05-09 19:16:41 +0530

    Update Higher_Order_Function.py

--- 019/Etch-A-Sketch Project/Higher_Order_Function.py
+++ 019/Etch-A-Sketch Project/Higher_Order_Function.py
@@ -1,14 +1,10 @@
-# Turtle Listen Method: https://docs.python.org/3/library/turtle.html#turtle.listen
-
 from turtle import Turtle, Screen
 
 tim = Turtle()
 screen = Screen()
 
-
 def move_forwards():
     tim.forward(10)
-
 
 screen.listen()
 screen.onkey(key="space", fun=move_forwards)  # Function as Inputs
