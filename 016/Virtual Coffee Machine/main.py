66003e37b4467a7bb39d902cfce2e7eb2ba6efed
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-05-09 18:28:31 +0530

    Create The Coffee Machine using concepts of OOP

--- 016/Virtual Coffee Machine/main.py
+++ 016/Virtual Coffee Machine/main.py
@@ -1,7 +1,3 @@
-# Steve Jobs interview: https://www.rollingstone.com/culture/culture-news/steve-jobs-in-1994-the-rolling-stone-interview-231132/
-# Coffee-Machine Documentation: https://docs.google.com/document/d/e/2PACX-1vTragRHILyj76AvVgpWeOlEaLBXoxPM_43SdEyffIKtOgarj42SoSAsK6LwLAdHQs2qFLGthRZds6ok/pub
-
-
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
