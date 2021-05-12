1722b21325bc10c6f4f41c578d16681195f9699d
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-05-13 02:50:08 +0530

    Create main.py

---
+++ 047/Amazon Price Tracker Project/main.py
@@ -0,0 +1,41 @@
+import requests
+import lxml
+from bs4 import BeautifulSoup
+import smtplib
+
+EMAIL = "aniruddhupadhyay07@gmail.com"
+PASSWORD = "AnSh@482"
+
+URL = "https://www.amazon.com/Instant-Pot-Duo-Evo-Plus/dp/B07W55DDFB/ref=sr_1_1?qid=1597662463"
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.56",
+    "Accept-Language": "en-US,en;q=0.9"
+}
+
+response = requests.get(URL, headers=header)
+
+soup = BeautifulSoup(response.content, "lxml")
+print(soup.prettify())
+
+price = soup.find(id="priceblock_ourprice").get_text()
+price_without_currency = price.split("$")[1]
+price_as_float = float(price_without_currency)
+print(price_as_float)
+
+title = soup.find(id="productTitle").get_text().strip()
+print(title)
+
+BUY_PRICE = 100
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is now {price}"
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        result = connection.login(EMAIL, PASSWORD)
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs=EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{URL}"
+        )
