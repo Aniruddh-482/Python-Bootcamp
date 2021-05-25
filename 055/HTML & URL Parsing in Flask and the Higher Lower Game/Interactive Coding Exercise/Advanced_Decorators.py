99a7f00d37413230c5c89f9a9003058a29f808ce
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-05-26 03:19:54 +0530

    Interactive Coding Exercise: Advanced Decorators

---
+++ 055/HTML & URL Parsing in Flask and the Higher Lower Game/Interactive Coding Exercise/Advanced_Decorators.py
@@ -0,0 +1,15 @@
+# Create the logging_decorator() function 👇
+def logging_decorator(function):
+    def wrapper(*args, **kwargs):
+        print(f"You called {function.__name__}{args}")
+        result = function(args[0], args[1], args[2])
+        print(f"It returned: {result}")
+    return wrapper
+
+# Use the decorator 👇
+@logging_decorator
+def a_function(a, b, c):
+    return a * b * c
+
+a_function(1, 2, 3)
+
