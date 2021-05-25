3e4422ed9dd1ebb03e1641987c9b089d420959a3
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-05-26 03:02:50 +0530

    Advanced Python Decorator Functions

---
+++ 055/HTML & URL Parsing in Flask and the Higher Lower Game/Advanced Python Decorator Functions/main.py
@@ -0,0 +1,21 @@
+## Advanced Python Decorator Functions
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+
+def is_authenticated_decorator(function):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in == True:
+            function(args[0])
+    return wrapper
+
+@is_authenticated_decorator
+def create_blog_post(user):
+    print(f"This is {user.name}'s new blog post.")
+
+new_user = User("angela")
+new_user.is_logged_in = True
+create_blog_post(new_user)
+
